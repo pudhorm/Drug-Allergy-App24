bf7c7d4a4ded5bc06461ddef17a771f10b2ff8a2
pudhorm <pudhorm.ph@gmail.com>
2025-11-01 00:10:35 +0700

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,3 +1,5 @@
 // page2.js
-document.addEventListener("DOMContentLoaded", () => {});
-
+window.renderPage2 = function () {
+  const el = document.getElementById("page2");
+  el.innerHTML = `<h2>หน้า 2 ระบบอื่น</h2><p class="small-note">เดี๋ยวเราจะมาใส่ฟอร์มทีหลัง</p>`;
+};
