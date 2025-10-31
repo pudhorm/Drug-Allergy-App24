049f02042f7a1eb1454191eda7c15548d3979555
pudhorm <pudhorm.ph@gmail.com>
2025-11-01 00:10:53 +0700

    Update page3.js

--- page3.js
+++ page3.js
@@ -1,1 +1,6 @@
+// page3.js
+window.renderPage3 = function () {
+  const el = document.getElementById("page3");
+  el.innerHTML = `<h2>หน้า 3 Lab</h2><p class="small-note">เดี๋ยวเราจะมาใส่ช่องกรอก Lab ทีหลัง</p>`;
+};
 
