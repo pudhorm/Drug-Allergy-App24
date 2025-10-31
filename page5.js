4fb5911a258171a385f557675fec254f10f8d594
pudhorm <pudhorm.ph@gmail.com>
2025-11-01 00:11:32 +0700

    Update page5.js

--- page5.js
+++ page5.js
@@ -1,1 +1,6 @@
+// page5.js
+window.renderPage5 = function () {
+  const el = document.getElementById("page5");
+  el.innerHTML = `<h2>หน้า 5 Timeline</h2><p class="small-note">หน้านี้เก็บวันที่เริ่มยา/วันที่เริ่มมีอาการ</p>`;
+};
 
