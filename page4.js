9ab63a5c1924b7263708be8faf85e3cef2f0b11e
pudhorm <pudhorm.ph@gmail.com>
2025-11-01 00:11:14 +0700

    Update page4.js

--- page4.js
+++ page4.js
@@ -1,1 +1,6 @@
+// page4.js
+window.renderPage4 = function () {
+  const el = document.getElementById("page4");
+  el.innerHTML = `<h2>หน้า 4 Naranjo</h2><p class="small-note">หน้านี้เก็บข้อมูล ไม่ได้ใช้คำนวณหลัก</p>`;
+};
 
